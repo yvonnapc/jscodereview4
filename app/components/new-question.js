f7c4f1a8cc2c780ee7158909a47595a6a79fc4e7
Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-04-01 16:59:29 -0700

    Write saveQuestion function in index.js

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -5,6 +5,17 @@ export default Ember.Component.extend({
   actions: {
     questionFormShow(){
       this.set('addNewQuestion', true);
+    },
+
+    saveQuestion(){
+      var params = {
+        title: this.get('title'),
+        author: this.get('author'),
+        content: this.get('content'),
+        notes: this.get('notes'),
+      };
+      this.set('addNewQuestion', false);
+      this.sendAction('saveQuestion', params);
     }
   }
 });
