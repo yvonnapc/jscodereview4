27144110a925e4e3648839d4d367ce64cb6a4abc
Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-04-01 16:54:49 -0700

    Add new question form and function

---
+++ app/components/new-question.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow(){
+      this.set('addNewQuestion', true);
+    }
+  }
+});
