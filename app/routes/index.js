e0693688fb1984e3a8381a4f2b4ee18c3c502b3d
Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-04-01 16:31:06 -0700

    Update the model hook to return all from database

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,21 +1,7 @@
 import Ember from 'ember';
 
-var questions = [{
-  id: 1,
-  title: "Portland Food",
-  author: "Alex Rivera",
-  content: "Where is the best food on Mississippi St.?",
-  note: "Willing to drive",
-}, {
-  id: 2,
-  title: "Adventure Time",
-  author: "Yvonna Contreras",
-  content: "Is Adventure Time better than Spongebob?",
-  note: "I am 12",
-}];
-
 export default Ember.Route.extend({
   model(){
-    return questions;
+    return this.store.findAll('question');
   },
 });
